e0d78a950e0634f971864f8186bb8da94acc3ec4
Patricio <patricioprp06@gmail.com>
2017-11-10 21:39:29 -0300

    se agregan migraciones contratos y reparticiones

--- database/migrations/2017_11_08_152908_add_empleados_table.php
+++ database/migrations/2017_11_08_152908_add_empleados_table.php
@@ -18,10 +18,11 @@ class AddEmpleadosTable extends Migration
             $table->bigInteger('dni')->unique();
             $table->string('nombre');
             $table->string('apellido');
+            $table->enum('programa',['MDP'],['UEDP'],['PRODERI'],['PROICSA']);
             $table->integer('contrato_id')->unsigned(); /*unsigned es para setear el campo de la tabla*/
             $table->integer('distribution_id')->unsigned();
 
-            
+
 
             $table->timestamps();
         });
---
+++ database/migrations/2017_11_10_232812_add_contratos_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddContratosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contratos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('fondos_origen');
+            $table->integer('indicador');
+            $table->double('monto',8,2);
+            $table->integer('duracion');
+            $table->enum('estado',['finalizado'],['activo'],['proximo']);
+            $table->string('tipo');
+            $table->string('actividad');
+            $table->date('desde');
+            $table->date('hasta');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contratos');
+    }
+}
