32d0f8c109317c94da1607c1a83a6b9ccba4829f
patricio <patricioprp06@gmail.com>
2017-11-14 12:57:13 -0300

    se agrega las claves foraneas

--- database/migrations/2017_11_08_152908_add_empleados_table.php
+++ database/migrations/2017_11_08_152908_add_empleados_table.php
@@ -18,12 +18,20 @@ class AddEmpleadosTable extends Migration
             $table->bigInteger('dni')->unique();
             $table->string('nombre');
             $table->string('apellido');
+            $table->enum('estado',['ACTIVO'],['INACTIVO']);
             $table->enum('programa',['MDP'],['UEDP'],['PRODERI'],['PROICSA']);
             $table->integer('contrato_id')->unsigned(); /*unsigned es para setear el campo de la tabla*/
             $table->integer('distribution_id')->unsigned();
 
-
-
+            $table->foreign('contrato_id')->references('id')->on('contratos');
+            /*contrato_id de la tabla empleado hace referencia al campo id de la
+            tabla contratos*/
+            $table->foreign('distribution_id')->references('id')->on('distributions');
+            /*
+          $table->foreign('contrato_id')->references('id')->on('contratos')->onDelete('cascade');
+          EN ESTE CASO ->ONDELETE('CASCADE') HACE Q CUANDO SE ELIMINE UN EMPLEADO SE ELIMINEN SUS CONTRATOS
+          EN ESTE CASO NO SE BUSCA ESA FUNCION POR ESO NO SE LA USA
+            */
             $table->timestamps();
         });
     }
