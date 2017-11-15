10aa5b8f13c0da46e12cdec9abf2916ecaecc85e
patricio <patricioprp06@gmail.com>
2017-11-15 11:49:13 -0300

    se agrega campos

--- database/migrations/2017_11_10_232812_add_contratos_table.php
+++ database/migrations/2017_11_10_232812_add_contratos_table.php
@@ -14,6 +14,7 @@ class AddContratosTable extends Migration
     public function up()
     {
         Schema::create('contratos', function (Blueprint $table) {
+
             $table->increments('id');
             $table->string('fondos_origen');
             $table->integer('indicador');
@@ -25,10 +26,14 @@ class AddContratosTable extends Migration
             $table->date('desde');
             $table->date('hasta');
             $table->integer('empleado_id')->unsigned();
+
             $table->foreign('empleado_id')->references('id')->on('empleados');
             $table->timestamps();
         });
+
+
     }
+
 
     /**
      * Reverse the migrations.
