461e9657e45ec80eb272a70334c150e501a15424
Patricio <patricioprp06@gmail.com>
2017-12-21 01:53:46 -0300

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,12 @@
 @section('title','Gestion de contratos')
 
 @section('content')
-  <h1>Esto es bootstrap</h1>
-  <a href="" class="btn btn-success" > Boton de Bootstrap</a>
+  <div class="center"><h1>SISTEMA WEB PARA SEGUIMIENTO DE CONTRATOS</h1></div>
+  <h2>PUNTOS A TENER EN CUENTA:</h2>
+  <ul>
+    <li><h3>Si se quiere eliminar una Reparticion o Programa con el cual se dio de alta un Consultor, se debe eliminar antes
+    el Consultor y luego eliminar el Programa o Reparticion, de lo contrario se puede editar el Programa o Reparticion y automaticamente
+  se modificaran dichas propiedad del Consultor.</h3></li>
+    <li><h3>Solo los Usuarios con privilegios de ADMINISTRADOR y ademas que esten logueados tendran acceso a la informarcion del sistema.</li>
+  </ul>
 @endsection
