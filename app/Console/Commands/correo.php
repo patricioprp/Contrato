a9ae897479833c1c06bfd956265d82ca65a1844b
Patricio Polito <patricioprp06@gmail.com>
2018-02-21 12:42:28 -0300

    se automatiza el indicador para los estado del contrato

--- app/Console/Commands/correo.php
+++ app/Console/Commands/correo.php
@@ -3,7 +3,9 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+use App\Contrato;
 class correo extends Command
 {
     /**
@@ -37,6 +39,34 @@ class correo extends Command
      */
     public function handle()
     {
-        echo "enviando el correo";
+        $contratos = Contrato::all();
+
+         foreach ($contratos as $contrato)
+         {
+        $fechaDesde = Carbon::parse($contrato->desde);
+        $fechaHasta = Carbon::parse($contrato->hasta);
+
+        $diasDiferencia = $fechaHasta->diffInDays($fechaDesde);
+        $contrato->indicador=$diasDiferencia;
+
+        //volver a consultar las fechas para los estados
+        if($contrato->indicador < 16)
+        {
+            $contrato->estado="proximo";
+        }
+        else
+            if($contrato->indicador>15)
+            {
+                $contrato->estado="activo";
+            }
+            else
+                if($contrato->indicador<=0)
+                {
+                    $contrato->estado="finalizado";
+                }
+
+        $contrato->save();
+        }
+         
     }
 }
