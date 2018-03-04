48c7118dd43df7c802f10800a5183cd84052867d
Patricio <patricioprp06@gmail.com>
2018-03-04 10:16:39 -0300

    se mejora presentacion de blade en contratos

--- resources/views/admin/contrato/edit.blade.php
+++ resources/views/admin/contrato/edit.blade.php
@@ -16,24 +16,24 @@
        <div class="col-lg-8">
   {!! Form::text('fondos_origen',$contrato->fondos_origen,['class' => 'form-control', 'placeholder'=>'Fondos de Origen','required']) !!}
 </div></div>
-       <div class="form-group col-lg-8">
-  {!! Form::label('monto','Monto($)',['class'=>'col-lg-1 control-label']) !!}
-           <div class="col-lg-8">
-  {!! Form::text('monto',$contrato->monto,['class' => 'form-control', 'placeholder'=>'Monto','required']) !!}
+       <div class="form-group col-lg-6">
+  {!! Form::label('monto','Monto',['class'=>'col-lg-1 control-label']) !!}
+           <div class="col-lg-6">
+  {!! Form::text('monto',$contrato->monto,['class' => 'form-control', 'placeholder'=>'Monto($)','required']) !!}
 </div></div>
        <div class="form-group col-lg-6">
   {!! Form::label('estado','Estado',['class'=>'col-lg-1 control-label']) !!}
-               <div class="col-lg-8">
+               <div class="col-lg-6">
   {!! Form::select('estado',['' => 'SELECCIONE EL ESTADO', 'activo'=>'activo','proximo'=>'proximo','finalizado'=>'finalizado'],$contrato->estado,['class'=>'form-control select-estado','multiple','required']) !!}
 </div></div>
-       <div class="form-group col-lg-12">
-  {!! Form::label('tipo','Tipo(Obra,Serv., etc.)',['class'=>'col-lg-1 control-label']) !!}
-    <div class="col-lg-8">
+       <div class="form-group col-lg-6">
+  {!! Form::label('tipo','Tipo',['class'=>'col-lg-1 control-label']) !!}
+    <div class="col-lg-6">
   {!! Form::text('tipo',$contrato->tipo,['class' => 'form-control', 'placeholder'=>'tipo','required']) !!}
 </div></div>
-         <div class="form-group col-lg-8">
+         <div class="form-group col-lg-6">
   {!! Form::label('actividad','Actividad',['class'=>'col-lg-1 control-label']) !!}
-      <div class="col-lg-8">
+      <div class="col-lg-6">
   {!! Form::text('actividad',$contrato->actividad,['class' => 'form-control', 'placeholder'=>'actividad','required']) !!}
 </div></div>
          <div class="form-group col-lg-6">
