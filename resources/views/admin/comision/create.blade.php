f484c0a279573f762832c1cbc8e721d730acb7d8
Patricio Polito <patricioprp06@gmail.com>
2018-03-22 12:57:19 -0300

    se mejora crud de comisiones

--- resources/views/admin/comision/create.blade.php
+++ resources/views/admin/comision/create.blade.php
@@ -2,8 +2,13 @@
 @section('title','Crear Comision')
 @section('content')
 @section('contrato','active')
+  <div class="panel panel-default">
+    <!-- Default panel contents -->
+    <div class="panel-heading"><h3><p>Informacion del Consultor: <h2>{{$contrato->empleado->nombre}}  {{$contrato->empleado->apellido}}  |  Reparticion:{{$contrato->empleado->distribution->nombre}} | Programa: {{$contrato->empleado->programa->nombre}} </h2></p></h3></div>
+    <div class="panel-body">
+    </div>
   {!! Form::open(['route' => ['comision.store',$contrato],'method'=>'POST']) !!}
-
+<div class="form-group">
   <div class="form-group col-lg-6">
 {!! Form::label('desde','desde',['class'=>'col-lg-1 control-label']) !!}
 <div class="col-lg-6">
